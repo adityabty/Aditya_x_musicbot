7fce4c6c7f636cda6fd904562362f250f150984b
adityabty <satyabhamabehera5931@gmail.com>
2025-09-16 08:37:03 +0530

    Update start.py

--- AloneX/utils/inline/start.py
+++ AloneX/utils/inline/start.py
@@ -33,7 +33,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+            #InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
             InlineKeyboardButton(
                        text="˹ 𝐎ᴡɴᴇʀ ˼", user_id=config.OWNER_ID
                     ),
